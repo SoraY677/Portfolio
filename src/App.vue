d799baceac1601457956d4a69649ffb410cde05b
SoraY677 <SoraY677@outlook.com>
2019-08-02 11:40:12 +0900

    one page trans state

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StateController/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StateController from './components/StateController.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    StateController
   }
 }
 </script>
@@ -22,7 +21,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-size:20px;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/StateController.vue
@@ -0,0 +1,35 @@
+<!--StateController.Vue-->
+<template>
+  <!-- ページ遷移を管理(this.page_state)-->
+
+  <div id="state-controller">
+    <div v-bind:is="transComponents"></div>
+  </div>
+</template>
+
+<script>
+import TopPage from "./TopPage.vue"; 
+
+export default {
+  name:"state-contoroller",
+  data(){
+    return{
+      page_state : 0
+    }
+  },
+  components: {
+    /*コンポーネント一覧 */
+    "pagestate-0" :TopPage
+  },
+  computed:{
+    //コンポーネント遷移
+    transComponents: function(){
+      return "pagestate-" + this.page_state;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
